fe763fcadda365454d30feaede118c1a19119f50
semyers189 <semyers@fullsail.edu>
2021-10-16 22:42:51 -0400

    fixed bot presence

--- events/ready.js
+++ events/ready.js
@@ -2,10 +2,12 @@
 module.exports = async (client) => {
   console.log('Penny is online!');
   client.user.setPresence({
-    status: 'dnd', //You can show online, idle....
-    activity: {
-      name: '$help', //The message shown
-      type: 'PLAYING', //PLAYING: WATCHING: LISTENING: STREAMING:
-    },
+    status: 'online', //You can show online, idle....
+    activities: [
+      {
+        name: '$help', //The message shown
+        type: 'PLAYING', //PLAYING: WATCHING: LISTENING: STREAMING:
+      },
+    ],
   });
 };
