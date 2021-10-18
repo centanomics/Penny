1e360acf63160e30417718a33ea3c84a90e5235f
semyers189 <semyers@fullsail.edu>
2021-10-18 00:56:52 -0400

    added defaults model and defaultRoles command

---
+++ commands/mod/defaultRoles.js
@@ -0,0 +1,11 @@
+// @command     defaultRoles
+// @desc        Manages permissions for the bot
+// @access      mod
+
+module.exports = {
+  name: 'defaultRoles',
+  description: 'This makes the bot reply pong!',
+  delay: 0,
+  mod: true,
+  execute: async (message, args) => {},
+};
