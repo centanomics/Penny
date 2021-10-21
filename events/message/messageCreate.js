7498da05a4207a4ba8c203e6669c9021f585397f
semyers189 <semyers@fullsail.edu>
2021-10-21 11:16:49 -0400

    added mod checking for commands

--- events/message/messageCreate.js
+++ events/message/messageCreate.js
@@ -8,8 +8,16 @@ module.exports = async (client, message) => {
   const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
 
-  if (client.commands.get(command)) {
-    await client.commands.get(command).execute(message, args);
+  const cmdObject = client.commands.get(command);
+  if (cmdObject) {
+    // checks if command needs mod role and if user had administrator role
+    // runs if cmdmod is true and usermod is true or if only cmdmod is false
+    const userMod = message.member.permissions.has('ADMINISTRATOR');
+    if (cmdObject.mod === userMod || cmdObject.mod === false) {
+      await cmdObject.execute(message, args);
+    } else {
+      message.channel.send("You don't have the perms to do this btw!");
+    }
   } else {
     // if the command doesn't exist, notify the user
     message.channel.send(`${command} command does not exist`);
