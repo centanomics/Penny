e6651d509ab4e4a2d31597b007eb76b381f9faed
semyers189 <semyers@fullsail.edu>
2021-10-21 10:45:44 -0400

    updated defaultrole command to account for different situations

--- commands/mod/defaultrole.js
+++ commands/mod/defaultrole.js
@@ -11,17 +11,50 @@ module.exports = {
   delay: 0,
   mod: true,
   execute: async (message, args) => {
-    try {
-      const role = message.mentions.roles.first();
-      const newDefault = new Defaults({
-        guildId: message.guildId,
-        roleId: role.id,
-      });
+    const role = message.mentions.roles.first();
 
-      await newDefault.save();
-      message.channel.send(
-        `Made the ${role.name} role the default role of the server`
-      );
+    try {
+      const defRole = await Defaults.findOne({ guildId: message.guildId });
+
+      // if there are no arguments, return what the default role is
+      if (args.length === 0) {
+        const dRole = await message.guild.roles.fetch(defRole.roleId);
+        message.channel.send(`The default role is the ${dRole.name} role.`);
+        return;
+      }
+
+      // if the role ids are the same quit the function
+      if (role.id === defRole.roleId) {
+        message.channel.send(
+          `The ${role.name} role is already the default role!`
+        );
+        return;
+      }
+
+      // if a role already exists for this server, update it
+      // if not create a role in the database
+      if (defRole) {
+        const roleFields = {
+          roleId: role.id,
+        };
+
+        const updatedDefault = await Defaults.findByIdAndUpdate(
+          defRole._id,
+          { $set: roleFields },
+          { new: true }
+        );
+        message.channel.send(`Updated the default role to ${role.name}`);
+      } else {
+        const newDefault = new Defaults({
+          guildId: message.guildId,
+          roleId: role.id,
+        });
+
+        await newDefault.save();
+        message.channel.send(
+          `Made the ${role.name} role the default role of the server`
+        );
+      }
     } catch (err) {
       console.log(err.message);
     }
---
+++ events/members/guildMemberAdd.js
@@ -0,0 +1,11 @@
+// Does something when a user joins a guild.
+
+const Defaults = require('../../models/defaults');
+
+module.exports = async (client, member) => {
+  try {
+    // const
+  } catch (err) {
+    console.log(err.message);
+  }
+};
