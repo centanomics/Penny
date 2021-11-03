2b429d598909391a9fc51213892a07cc2c9a5847
semyers189 <semyers@fullsail.edu>
2021-11-03 04:41:38 -0400

    creates invites in database on creation

--- events/invites/inviteCreate.js
+++ events/invites/inviteCreate.js
@@ -1,3 +1,14 @@
+const Invites = require('../../models/invites');
+
 module.exports = async (client, invite) => {
-  console.log('invite created');
+  try {
+    const newInvite = new Invites({
+      inviteId: invite.code,
+      guildId: invite.guild.id,
+    });
+
+    await newInvite.save();
+  } catch (err) {
+    console.log(err.message);
+  }
 };
