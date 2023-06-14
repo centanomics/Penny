e92d8c4e79148cf2aa2a69f5e7d5ed1ff428c443
semyers189 <semyers@fullsail.edu>
2023-06-13 20:02:00 -0400

    prepping poll test, mocks, etc

---
+++ __tests__/commands/poll.test.js
@@ -0,0 +1,9 @@
+const { getMessageMock } = require('../../__mocks__');
+
+describe('poll tests', () => {
+  const msgMock = getMessageMock();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+});
